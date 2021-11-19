01fffc79162a12dc43cec685833967070fee48e1
BennyZ3 <bennyz3@hotmail.com>
2021-11-19 11:59:48 -0500

    layout for checkout form

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,6 +1,22 @@
 //Checkout form componenet probably
 const Checkout = (props) => {
-  return <></>;
+  return (
+    <>
+      <form>
+        <label for="first">First Name </label>
+        <input type="text" id="first" name="first" />
+        <label for="last">Last Name </label>
+        <input type="text" id="last" name="last" />
+        <label for="email">Email </label>
+        <input type="email" id="email" name="email" />
+        <label for="credit">Credit Card</label>
+        <input type="number" id="credit" name="credit" />
+        <label for="zip">Zip Code</label>
+        <input type="number" id="zip" name="zip" />
+        <button type="submit">Buy Now</button>
+      </form>
+    </>
+  );
 };
 
 export default Checkout;
