17007379d3829f930b813cf93477784f5e4697a8
BennyZ3 <bennyz3@hotmail.com>
2021-11-19 12:05:26 -0500

    adjusted formating for 01 prodcut test

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 //Import product data and for each here
 import productData from "../data/productData";
+import formatPrice from "../helpers/formatPrice";
 const Item = (props) => {
   let itemArr = [];
   productData.forEach((item) => {
@@ -7,7 +8,7 @@ const Item = (props) => {
     itemArr.push(
       <div>
         <h2>{item.name}</h2>
-        <p>${item.price}</p>
+        <p>Price: {formatPrice(item.price)}</p>
         <button onClick={props.addToCart} name={item.name} value={item.price}>
           Add To Cart
         </button>
